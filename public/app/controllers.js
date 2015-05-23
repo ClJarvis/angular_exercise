ad4b8f9f9ef34a3364fee06c6afdf1d21ca517a3
Chris Jarvis <clj@ollure.com>
2015-05-23 17:10:01 -0500

    basic site works

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -2,4 +2,18 @@ var controllers = angular.module('myapp.controllers', []);
 
 controllers.controller('IndexController', ['$scope', function($scope){
 	$scope.message = "Hello I am the Home page";
-}])
+}]);
+
+controllers.controller('AboutController', ['$scope', function($scope){
+	$scope.message = "Hello I am the About page";
+}]);
+
+
+controllers.controller('NeedsController', ['$scope', function($scope){
+	$scope.message = "Hello I am the Needs page";
+	$scope.needs=[];
+	$scope.addNeeds = function(thingy){
+		$scope.needs.push(thingy);
+		$scope.newNeed='';
+	};
+}]);
